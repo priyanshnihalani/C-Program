8d892226d05853fc7fa42221096108c90d568fb8
priyanshnihalani-169 <priyansh.nihalani@gmail.com>
2024-02-09 17:24:37 +0530

    C# Programs

---
+++ Bitwise.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program{
+    static void Main(){
+        int a = 10;
+        int b = 12;
+        int c = a << 2;
+        int d = a >> 2;
+        int e = a & b;
+        int f = a | b;
+        int g = ~ b;
+        int h = a ^ b;
+        Console.WriteLine(h);
+    }
+}
---
+++ bitwiseoperator.cs
@@ -0,0 +1,9 @@
+    using System;
+    class Program
+    {
+        static void Main(){
+            int  n = 10, m = 5;
+            int result  = n & m;
+            Console.WriteLine(result);
+        }    
+    }
